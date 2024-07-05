029b9c58be1396282ec8b7759dff9ff8907687e7
juandamunozs <juandavidmunozsotelo7@gmail.com>
2024-07-05 14:43:58 -0500

    Cambios agregando todas las canciones e identado, documentando y mejorando el orden en los archivos y carpetas

---
+++ Js/evento_audio.js
@@ -0,0 +1,302 @@
+//Evento de audio de la fecha actual
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var now = document.querySelector('.container-actual');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/now.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    now.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de el himno de seleccion colombia
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerColombia = document.querySelector('.container-colombia');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/colombia.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerColombia.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+
+//Evento de audio de el himno del real madrid
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerRealMadrid = document.querySelector('.container-real_madrid');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/real_madrid.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerRealMadrid.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+
+//Evento de audio de san valentin
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerSanValentin = document.querySelector('.container-san-valentin');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/san_valentin.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerSanValentin.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+
+//Evento de audio hbk
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerHB = document.querySelector('.container-love');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/hb.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerHB.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento del audio hallowwen
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerHalloween = document.querySelector('.container-halloween');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/halloween.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerHalloween.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de hbm
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerHB = document.querySelector('.container-hb');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/hb.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerHB.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de pensum
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerGraduation = document.querySelector('.container-graduation');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/graduation.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerGraduation.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de navidad
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerNavidad = document.querySelector('.container-navidad');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/navidad.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerNavidad.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de año nuevo
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerAnioNuevo = document.querySelector('.container-anionuevo');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/anio_nuevo.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerAnioNuevo.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de party
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerParty = document.querySelector('.container-party');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/party.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerParty.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
+
+//Evento de audio de cancion recomendada
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el contenedor de Año Nuevo
+    var containerRecomendada = document.querySelector('.container-cancion_recomendada');
+
+    // Crea un nuevo elemento de audio
+    var audio = new Audio('/Audios/recomendada.mp3');
+
+    // Variable para controlar el estado de reproducción
+    var isPlaying = false;
+
+    // Agrega el EventListener para el clic
+    containerRecomendada.addEventListener('click', function () {
+        if (!isPlaying) {
+            // Si no está reproduciendo, inicia la reproducción
+            audio.play();
+            isPlaying = true;
+        } else {
+            // Si está reproduciendo, pausa la reproducción
+            audio.pause();
+            isPlaying = false;
+        }
+    });
+});
